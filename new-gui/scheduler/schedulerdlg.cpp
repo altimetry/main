cef2d7c214be5af8b70318e0af0ab40a9947df04
brat <fermart@clix.pt>
2015-11-12 20:37:53 +0000

    Added resources. Developments on the class constructor.

--- new-gui/scheduler/schedulerdlg.cpp
+++ new-gui/scheduler/schedulerdlg.cpp
@@ -1,7 +1,59 @@
+// Include private headers
+
+#include <locale>
+#include <string>
+
+//#include "Trace.h"
+//#include "List.h"
+//#include "Tools.h"
+//#include "Exception.h"
+//using namespace brathl;
+
+//#include "BratScheduler.h"
+//#include "SchedulerTaskConfig.h"
+//#include "RichTextFrame.h"
+//#include "DirTraverser.h"
+
+
+// When debugging changes all calls to new to be calls to DEBUG_NEW allowing for memory leaks to
+// give you the file name and line number where it occurred.
+// Needs to be included after all #include commands
+#include "Win32MemLeaksAccurate.h"
+
+#ifdef WIN32
+const std::string BRATHL_ICON_FILENAME = "BratIcon.ico";
+#else
+const std::string BRATHL_ICON_FILENAME = "BratIcon.bmp";
+#endif
+
+//const wxString BRATGUI_APPNAME = "BratGui";
+//const wxString GROUP_COMMON = "Common";
+//const wxString ENTRY_USER_MANUAL = "UserManual";
+//const wxString ENTRY_USER_MANUAL_VIEWER = "UserManualViewer";
+
+
+
 #include "schedulerdlg.h"
 
 SchedulerDlg::SchedulerDlg(QWidget *parent) :
-    QDialog(parent)
+    QDialog(parent), mIsDialog( parent ? true : false )
 {
     setupUi(this);
+
+    if (mIsDialog)
+    {
+        setWindowTitle( "Brat Scheduler Dialog");
+    }
+    else
+    {
+        setWindowTitle( "Brat Scheduler");
+    }
+
+    // Single Instance Checker TODO!!!!!
+
+    // To be sure that number have always a decimal point (and not a comma
+    // or something else)
+    setlocale(LC_NUMERIC, "C");
+    
+
 }
--- new-gui/scheduler/schedulerdlg.h
+++ new-gui/scheduler/schedulerdlg.h
@@ -3,9 +3,21 @@
 
 #include "ui_schedulerdlg.h"
 
+
 class SchedulerDlg : public QDialog, private Ui::SchedulerDlg
 {
     Q_OBJECT
+
+    // types
+
+    // static data members
+
+    static const int32_t smSCHEDULER_TIMER_INTERVAL = 30000;
+    static const int32_t smCHECK_CONFIGFILE_TIMER_INTERVAL = 5000;
+
+    // instance data members
+
+    bool mIsDialog = false;
 
 public:
     explicit SchedulerDlg(QWidget *parent = 0);
