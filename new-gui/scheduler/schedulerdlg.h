213e7e16473922abe8fe75efe8011c13d9fa80e2
Fernando Martinho <fernando.martinho@deimos.com.pt>
2015-11-13 21:11:17 +0000

    Improvements in scheduler projects (qtc and vs)

--- new-gui/scheduler/schedulerdlg.h
+++ new-gui/scheduler/schedulerdlg.h
@@ -14,8 +14,8 @@ class SchedulerDlg : public QDialog, private Ui::SchedulerDlg
 
     // static data members
 
-    static const int32_t smSCHEDULER_TIMER_INTERVAL = 30000;
-    static const int32_t smCHECK_CONFIGFILE_TIMER_INTERVAL = 5000;
+    static const auto smSCHEDULER_TIMER_INTERVAL = 30000;
+    static const auto smCHECK_CONFIGFILE_TIMER_INTERVAL = 5000;
 
     // instance data members
 
